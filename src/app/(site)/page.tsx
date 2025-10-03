db5c6dca8a1bbb5acd14d63c05950a79e68fed83
kistdeov <kistuttidavidka@gmail.com>
2025-10-03 13:13:15 +0200

    iconfix3

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   title: "Okos Mail",
   description:
     "AI által vezérelt automata levelező program",
-  icons: "/favicon.ico",
+  icons: "images/icon.ico",
 };
 
 export default function Home() {
