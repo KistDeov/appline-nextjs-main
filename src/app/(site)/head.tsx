cf634974d3e63395dfd5a2f0fa5fb6f10500aade
kistdeov <kistuttidavidka@gmail.com>
2025-10-03 13:38:51 +0200

    iconfix7

--- src/app/(site)/head.tsx
+++ src/app/(site)/head.tsx
@@ -1,12 +1,12 @@
 export default function Head() {
   return (
     <>
-      {/* Primary favicon (browsers and Google often request /favicon.ico) */}
-      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      <link rel="shortcut icon" href="/favicon.ico" />
+  {/* Primary favicon (versioned to force browsers to fetch updated file) */}
+  <link rel="icon" href="/favicon-v2.ico" type="image/x-icon" />
+  <link rel="shortcut icon" href="/favicon-v2.ico" />
 
       {/* Fallbacks (keep the image in case you want a separate path) */}
-      <link rel="icon" href="/images/icon.ico" />
+  <link rel="icon" href="/images/icon.ico" />
       <link rel="apple-touch-icon" href="/images/icon.ico" />
 
       {/* Theme and tile color hints */}
--- src/app/(studio)/head.tsx
+++ src/app/(studio)/head.tsx
@@ -1,8 +1,8 @@
 export default function Head() {
   return (
     <>
-      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="icon" href="/favicon-v2.ico" type="image/x-icon" />
+  <link rel="shortcut icon" href="/favicon-v2.ico" />
       <link rel="icon" href="/images/icon.ico" />
       <link rel="apple-touch-icon" href="/images/icon.ico" />
       <meta name="theme-color" content="#000000" />
